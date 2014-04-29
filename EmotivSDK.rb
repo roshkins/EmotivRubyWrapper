7b30d8a8e6aa5dfef629f12946d0c30170336bb0
Rashi Abramson <roshkins@gmail.com>
2014-04-28 20:36:06 -0700

    Fixed segfault in ruby code

--- EmotivSDK.rb
+++ EmotivSDK.rb
@@ -1,22 +1,33 @@
 require 'ffi'
 
-module EmotivSDK
+module EmotivNative
   extend FFI::Library
   EDK_OK = 0
   ffi_lib "./ext/libedk.so"
-  attach_function :EE_EngineRemoteConnect, [:string, :ushort], :int
+  #= FFI::Library 
+  #This is an internal SDK of the functions. It's not reccomended that you use
+  # this directly. Instead, use the EmotivSDK itself.
+  
+  # Notice that EE_EngineRemoteConnect has 3 parameters while the SDK docs lists
+  # two. Thats because there was a segfault when that wasn't filled in with a
+  # developer id for the last parameter.
+  attach_function :EE_EngineRemoteConnect, [:string, :ushort, :string], :int
   attach_function :EE_EngineConnect, [:string], :int
   attach_function :EE_EngineDisconnect, [], :int
-  
-  class Engine 
-  
-  def self.setupengine(simulator = false)
-    self.EE_EngineConnect "Emotiv Systems-5"
-    if simulator      
-      self.EE_EngineRemoteConnect("127.0.0.1", 1726)
-    end
-  end
-  
+  attach_function
 end
 
-EmotivSDK.setupengine(true)
+module EmotivSDK
+  class Engine   
+    include EmotivNative
+    def initialize(simulator = false)
+      if simulator      
+        puts self.EE_EngineRemoteConnect("127.0.0.1", 1726, "Emotiv Systems-5")
+      else
+         puts self.EE_EngineConnect "Emotiv Systems-5"
+      end
+    end 
+  end
+end
+
+puts EmotivSDK::Engine.new(true)
